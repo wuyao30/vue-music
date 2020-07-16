cc67d8875ba14c001aa4bee3edb7761a00f657f1
wuyao20 <huyashangding@163.com>
2020-07-16 11:04:06 +0800

    tab

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'babel-polyfill'
 import fastclick from 'fastclick'
 import Vue from 'vue'
 import App from './App'
+import router from './router'
 
 import 'common/stylus/index.styl'
 fastclick.attach(document.body)
@@ -12,5 +13,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
