ca03c7c30b7faf38cdcfe08de43e331748fa6415
wuyao20 <huyashangding@163.com>
2020-07-16 15:19:37 +0800

    jsonp

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -5,8 +5,29 @@
 </template>
 
 <script>
+import { ERR_OK } from '../../api/config'
+import { getRecommend } from '../../api/recommend'
+
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  data() {
+    return {
+      recommends: []
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+          console.log(this.recommends)
+        }
+      })
+    }
+  }
 }
 </script>
 
